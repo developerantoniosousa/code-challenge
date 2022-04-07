8804d1bffdf76cdc98e864bd9c5f4f386dae6c53
Antonio Sousa <developerantoniosousa@gmail.com>
2022-04-07 18:22:56 -0300

    improve array representation in tests description

--- binary.test.js
+++ binary.test.js
@@ -2,19 +2,19 @@ const binarySearch = require("./binary");
 
 const sequence = [1, 2, 3, 4, 5];
 
-test(`given ${sequence} and 1, returns 1`, () => {
+test(`given [${sequence}] and 1, returns 1`, () => {
   expect(binarySearch(sequence, 1)).toBe(1);
 });
 
-test(`given ${sequence} and 2, returns 2`, () => {
+test(`given [${sequence}] and 2, returns 2`, () => {
   expect(binarySearch(sequence, 2)).toBe(2);
 });
 
-test(`given ${sequence} and 3, returns 3`, () => {
+test(`given [${sequence}] and 3, returns 3`, () => {
   expect(binarySearch(sequence, 3)).toBe(3);
 });
 
-test(`given ${sequence} and 4, returns 4`, () => {
+test(`given [${sequence}] and 4, returns 4`, () => {
   expect(binarySearch(sequence, 4)).toBe(4);
 });
 
