20257552fec83261383ac6ff3256991434b885a8
Antonio Sousa <developerantoniosousa@gmail.com>
2022-04-07 18:30:51 -0300

    improve code quality

--- binary.js
+++ binary.js
@@ -1,10 +1,11 @@
 module.exports = function binarySearch(array, value) {
   let middleIndex = Math.floor(array.length / 2);
+  const middleValue = array[middleIndex];
 
-  if (value === array[middleIndex]) {
-    return array[middleIndex];
+  if (value === middleValue) {
+    return middleValue;
   }
-  if (value < array[middleIndex]) {
+  if (value < middleValue) {
     const leftSide = array.slice(0, middleIndex);
     return binarySearch(leftSide, value);
   } else {
